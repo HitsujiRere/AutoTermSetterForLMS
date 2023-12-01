8752b778de9539fd456b056d36f6ec74bc855694
HitsujiRere <55770125+HitsujiRere@users.noreply.github.com>
2023-12-01 10:17:29 +0900

    Change term only initially

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const handler = () => {
+  if (sessionStorage.getItem(`_${chrome.runtime.id}_initialized`)) {
+    return;
+  }
+
   const selectTerm = (term) => {
     const termSelecter = document.getElementsByName("search_term")[0];
     if (termSelecter && termSelecter.value === "" && term) {
       termSelecter.value = term;
       termSelecter.dispatchEvent(new Event("change"));
     }
+
+    sessionStorage.setItem(`_${chrome.runtime.id}_initialized`, term);
   };
 
   chrome.storage.sync.get(["term"], (res) => {
